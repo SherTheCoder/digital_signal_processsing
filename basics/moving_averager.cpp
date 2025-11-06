4b7833eb121b4990a6cd3c4ef57d07a65eafd4e7
SherAmir <sheramir51@gmail.com>
2025-11-07 00:15:24 +0530

    moving averager

--- basics/moving_averager.cpp
+++ basics/moving_averager.cpp
@@ -1,14 +1,80 @@
 #include <iostream>
-#include "wav_header.h"
-
+#include<fstream>
+#include "../wav_header.h"
 using namespace std;
 
+// an N point moving averager
+int averager(string pathName, int point){
+    // opening the file
+    ifstream input(pathName, ios::binary);
+    if(!input)
+        return -100;
+    WAVHeader header;
+    // read actually needs a character pointer, and the size of bytes to read
+    // reinterpret cast tells C++ to treat header as a character pointer, and trust it will all go well
+    input.read(reinterpret_cast<char*>(&header), sizeof(WAVHeader));
+    if(header.bitsPerSample == 64 || header.bitsPerSample == 8 || header.bitsPerSample == 24 || header.bitsPerSample == 32)
+        return -10; // it will overflow as the largest built in int we have is int64_t
+                    // Also, 8 bit and 24 bit are currently unsupported as they require
+                    // a piece of code to "sign extend" the int64_t variable we're using to read from input
+    
 
-int main(){
-	
+    ofstream output("output.wav", ios::out | ios::binary);
+    if(!output)
+        return -20;
+    output.write(reinterpret_cast<char*>(&header), sizeof(WAVHeader));
+    // This will be a circular buffer to hold the previous N samples
+    // and let us calculate a running average without having a second pointer in our input file
+    int numberOfChannels = header.numChannels;
+    vector<vector<int16_t>> prevSamples(numberOfChannels, vector<int16_t>(point, 0));
+    uint32_t prevSamplesIndex = 0;
+    // This will not be an unsigned as the sign matters when adding samples  
+    vector<int32_t> sampleSum(numberOfChannels, 0);
+    int bytesPerSample = header.bitsPerSample / 8;
+    cout << "sizeof(WAVHeader) = " << sizeof(WAVHeader) << "\n";
+cout << "riff = " << string(header.riff, 4) << "\n";
+cout << "wave = " << string(header.wave, 4) << "\n";
+cout << "fmtSize = " << header.fmtSize << "\n";
+cout << "bitsPerSample = " << header.bitsPerSample << "\n";
+cout << "data tag = " << string(header.data, 4) << "\n";
+cout << "dataBytes = " << header.dataBytes << "\n";
 
+    cout<< bytesPerSample<<endl;
+    cout<< numberOfChannels<<endl;
+    uint32_t totalSamples = header.dataBytes / (bytesPerSample * numberOfChannels);
+    
+
+    for(uint32_t i = 0 ; i < totalSamples; i++){
+        for(int ch = 0 ; ch < numberOfChannels; ch++){
+            int16_t sample = 0;
+            input.read(reinterpret_cast<char*>(&sample), bytesPerSample);
+            sampleSum[ch] -= prevSamples[ch][prevSamplesIndex % point];
+            sampleSum[ch] += sample;
+            prevSamples[ch][prevSamplesIndex % point] = sample;
+            int16_t avg = static_cast<int16_t> (sampleSum[ch] / point);
+            output.write(reinterpret_cast<char*>(&avg), bytesPerSample);
+        }
+        prevSamplesIndex++;
+    }
+
+    input.close();
+    output.close();
+    return 0;
+
+    
 }
 
-void averager(){
-    
+int main(){
+    string pathName;
+    int point;
+    cout<< "Enter the path of the .wav file: ";
+    cin>> pathName;
+    cout<< "What should be the grade of the averager? ";
+    cin>> point;
+    if(point <= 0)
+        cout<< "don't play";
+    int code = averager(pathName, point);
+    if(code != 0)
+        cout<< "something weird happened, code: " << code << endl;
+    return 0;
 }
